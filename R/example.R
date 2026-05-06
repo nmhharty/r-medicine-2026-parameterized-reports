b5a779144fb801e21c5f51be145ae1c7c5c0aa78
Nicole <79059401+nmhharty@users.noreply.github.com>
2026-05-06 16:04:04 -0600

    work on exercises during R/medicine session

--- R/example.R
+++ R/example.R
@@ -4,9 +4,7 @@ library(purrr)
 # Single report
 quarto_render(
   input = "R/example_final.qmd",
-  execute_params = list(
-    choose_continent = "Europe"
-  )
+  execute_params = list(choose_continent = "Europe")
 )
 
 # For all continents
@@ -18,5 +16,28 @@ walk(
     execute_params = list(
       choose_continent = .x
     )
-  )
+  ),
+  .progress = TRUE #tells you how much progress has been made in rendering all reports
+)
+
+
+
+### TESTING THIS ON MY OWN WITH report.qmd ##
+# Single report
+quarto_render(
+  input = "R/report.qmd",
+  execute_params = list(choose_year = "1972")
+)
+
+# For all continents
+walk(
+  .x = unique(gapminder$year),
+  .f = ~quarto_render(
+    input = "R/report.qmd",
+    output_file = glue("report_{.x}.html"),
+    execute_params = list(
+      choose_year = .x
+    )
+  ),
+  .progress = TRUE #tells you how much progress has been made in rendering all reports
 )
